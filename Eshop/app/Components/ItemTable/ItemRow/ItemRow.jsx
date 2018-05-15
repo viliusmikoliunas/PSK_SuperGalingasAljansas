51268f5a267462690688bca8f9de38381d62a0d8
Vilius Mikoliūnas <vilius.mikoliunas@gmail.com>
2018-05-15 11:27:20 +0300

    Item table now loads from DB instead of sample data file

--- Eshop/app/Components/ItemTable/ItemRow/ItemRow.jsx
+++ Eshop/app/Components/ItemTable/ItemRow/ItemRow.jsx
@@ -2,25 +2,30 @@ import React from 'react';
 import './ItemRowStyles.css';
 
 export default class ItemRow extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    
+
     render(){
-        const {params} = this.props;
+        const {title, cost, pictureLocation, itemCategories, itemTraits} = this.props;
+        const categoryString = itemCategories != null
+            ? itemCategories.map(category => {
+                return (category + "\r\n")
+            })
+            : "<No categories>"
+        
+        const traitString = itemTraits != null
+            ? itemTraits.map(trait => {
+                return (trait + "\r\n")
+            })
+            : "<No traits>"
+
         return(
             <tr className="itemRow">
-                <th scope="row">
-                    <img src={params.ImagePath}/>
-                </th>
-                <td>{params.Title}</td>
-                <td>{params.Categories.map(category => {
-                    return (category + "\r\n")
-                })}</td>
-                <td>{params.Cost}</td>
-                <td>{params.Traits.map(trait => {
-                    return (trait + "\r\n")
-                })}</td>
+                <td scope="row">
+                    <img src={pictureLocation}/>
+                </td>
+                <td>{title}</td>
+                <td>{cost}</td>
+                <td>{categoryString}</td>
+                <td>{traitString}</td>                
             </tr>
         )
     }
