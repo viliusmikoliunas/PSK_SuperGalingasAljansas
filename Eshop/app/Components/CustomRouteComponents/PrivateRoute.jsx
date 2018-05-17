874114b65126fdb3fa4edab38b0dde7f03168c0e
Vilius Mikoliūnas <vilius.mikoliunas@gmail.com>
2018-05-17 19:57:20 +0300

    privateRoute component updated to match token login shceme

--- Eshop/app/Components/CustomRouteComponents/PrivateRoute.jsx
+++ Eshop/app/Components/CustomRouteComponents/PrivateRoute.jsx
@@ -3,30 +3,36 @@ import { bindActionCreators } from 'redux'
 import {connect} from 'react-redux'
 import {Redirect, Route, withRouter} from 'react-router-dom'
 import RouteWithLayout from './RouteWithLayout'
+import parseJwt from '../../FunctionalComponents/jwt/parseJwt'
+
 
 class PrivateRoute extends Component {
 
     checkPrivilege(userRole, approvedRolesForThisPath){
-        const uppercaseRoles = approvedRolesForThisPath.map(title => title.toUpperCase());
-        const currentRole = userRole.toUpperCase();
-        return (uppercaseRoles.indexOf(currentRole) > -1);
+        const uppercaseRoles = approvedRolesForThisPath.map(title => title.toUpperCase())
+        const currentRole = userRole.toUpperCase()
+        return (uppercaseRoles.indexOf(currentRole) > -1)
     }
 
     render(){
-        const { component: Component, roles: permisedRoles, layout, ...rest } = this.props;
-        const currentRole = this.props.loggedIn
-            ? this.props.loggedIn.role : null;
-        
+        const { component: Component, roles: permisedRoles, layout, ...rest } = this.props
+
+        const token = localStorage.getItem('jwtToken')
+        let currentRole = null
+        if (token != null){
+            currentRole = parseJwt(token)['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']
+        }
+
         const canUserAccessThisRoute = 
-            currentRole && this.checkPrivilege(this.props.loggedIn.role, permisedRoles);
+            currentRole && this.checkPrivilege(currentRole, permisedRoles)
         
-            //if user has no role he is redirected to login page
-        //else means user doesn't have permission to access resource, thus he is redirected to start of admin
+        //if user has no role he is redirected to login page
+        //else means user doesn't have permission to access resource, thus he is redirected to back to main page
         const redirectPath = currentRole == null
-            ? '/login' : '/admin';
+            ? '/login' : '/'
         
         return (
-            <Route {...rest} render={(props) => (<RouteWithLayout layout={layout} component={Component} {...props}/>/*
+            <Route {...rest} render={(props) => (
                 this.props.loggingIn
                 ? null
                 : 
@@ -37,15 +43,14 @@ class PrivateRoute extends Component {
                         state: { from: props.location }
                     }}/>
                 )
-            */)} />
+            )} />
         )
     }   
 }
 
 const connectedPrivateRoute = connect(
     (state) => ({
-        loggingIn: state.LoginReducer.loading,
-        loggedIn: state.LoginReducer.userInfo
+        loggingIn: state.LoginReducer.loggingIn
     }),
     null
 )(PrivateRoute)
