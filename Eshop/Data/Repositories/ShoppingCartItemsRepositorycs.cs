5d65c8c42c5a7fd07a9515d4f9169890343050fd
Vilius Mikoliūnas <vilius.mikoliunas@gmail.com>
2018-05-23 18:52:39 +0300

    removed async call in repository

--- Eshop/Data/Repositories/ShoppingCartItemsRepositorycs.cs
+++ Eshop/Data/Repositories/ShoppingCartItemsRepositorycs.cs
@@ -20,13 +20,12 @@ namespace Eshop.Data.Repositories
                 cartItem => cartItem.ShoppingCartId.Equals(shoppingCartId) && cartItem.ItemId == itemId);
         }
 
-        public async void DeleteOrphans()
+        public void DeleteOrphans()
         {
             _dbContext.ShoppingCartItems
                 .RemoveRange(_dbContext.ShoppingCartItems
                     .Where(cartItem => string.IsNullOrEmpty(cartItem.ShoppingCartId)));
-            //nobody will use shopping cart items with no shopping cart so it is safe to make this async
-            await _dbContext.SaveChangesAsync(); 
+            _dbContext.SaveChanges(); 
         }
 
         public void RemoveRange(string shoppingCartId, List<int> itemId)
