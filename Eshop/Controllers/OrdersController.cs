ca1c4bd9548c18f3f969bd6cb9129a42a95e1c41
Vilius Mikoliūnas <vilius.mikoliunas@gmail.com>
2018-05-28 19:05:37 +0300

    some comments and fixes

--- Eshop/Controllers/OrdersController.cs
+++ Eshop/Controllers/OrdersController.cs
@@ -31,48 +31,14 @@ namespace Eshop.Controllers
             _ordersRepository = ordersRepository;
         }
 
-        private bool ValidCreditCardNumber(string number)
-        {
-            // accept only digits, dashes or spaces
-            Regex regex = new Regex(@"^[(0-9)-/s]+$");
-            if (!regex.IsMatch(number)) return false;
-
-            number = number.Replace("/", "").Replace(" ", "").Replace("-", ""); 
-
-            if (number.Length != 16) return false;
-
-            // The Luhn Algorithm
-            int sumOfDigits = number.Where((e) => e >= '0' && e <= '9')
-                .Reverse()
-                .Select((e, i) => ((int)e - 48) * (i % 2 == 0 ? 1 : 2))
-                .Sum((e) => e / 10 + e % 10);
-
-            return (sumOfDigits % 10) == 0;
-        }
-
-        private bool ValidCvv(string cvv)
-        {
-            Regex regex = new Regex(@"^\d{3}$");
-            if (regex.IsMatch(cvv))
-                return true;
-            return false;
-        }
-
-        private bool ValidExpirationDate(int year, int month)
-        {
-            if (year < DateTime.Now.Year) return false;
-            if (month < DateTime.Now.Month) return false;
-            return true;
-        }
-
         [HttpPost]
         [Authorize(Roles = UserRoleString.User)]
         public IActionResult HandlePurchase([FromBody] PaymentRequest paymentInfo)
         {
-            if (paymentInfo == null) return BadRequest("Something bad happenned");
+            if (!ModelState.IsValid) return BadRequest(ModelState);
 
             //validating body
-            if (paymentInfo.Ammount <= 0)
+            if (paymentInfo.Amount <= 0)
                 return BadRequest("Amount to pay must is invalid"); //change request code
             if (!ValidCreditCardNumber(paymentInfo.Number))
                 return BadRequest("Credit card info is invalid");
@@ -85,14 +51,14 @@ namespace Eshop.Controllers
             if (!ValidCvv(paymentInfo.Cvv))
                 return BadRequest("Card cvv security code is invalid");
 
-            //check if card is not expired
-            if (!ValidExpirationDate(paymentInfo.Exp_Year, paymentInfo.Exp_Month))
-                return BadRequest("Credit card is expired");
+            //check if card is not expired - not our job
+            //if (!ValidExpirationDate(paymentInfo.Exp_Year, paymentInfo.Exp_Month))
+            //    return BadRequest("Credit card is expired");
 
             //check if ammount matches shopping cart total
             var username = JWTtoken.GetUsernameFromToken(Request);
             var user = _ordersRepository.GetOrderingUser(username);
-            decimal ammount = (decimal)paymentInfo.Ammount / 100;
+            decimal ammount = (decimal)paymentInfo.Amount / 100;
             if (!_ordersRepository.PaymentEqualsShoppingCartSum(user, ammount))
                 return BadRequest("Payment ammount does not match the shopping cart total ammount");
 
@@ -107,27 +73,22 @@ namespace Eshop.Controllers
 
             newOrder = _ordersRepository.Add(newOrder);
             var orderedItems = _ordersRepository.GetOrderShoppingCartItems(user, newOrder.Id);
+            //should also remove said items from shopping cart items table 
+            //or don't add newOrder to db until purchase is successful
             _ordersRepository.AddOrderedItems(orderedItems);
             newOrder.OrderedItem = orderedItems;
             newOrder = _ordersRepository.Update(newOrder);
 
             //*****make payment*****
             var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://mock-payment-processor.appspot.com/v1/payment");
-            httpWebRequest.Credentials = new System.Net.NetworkCredential("technologines", "platformos");
+            httpWebRequest.Credentials = new NetworkCredential("technologines", "platformos");
             httpWebRequest.ContentType = "application/json";
             httpWebRequest.Method = "POST";
 
             using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
             {
                 //harcodinta. greiciausiai reiketu pakeisti
-                string json = "{" +
-                              "\"amount\": 999," +
-                              "\"number\": \"4111111111111111\"," +
-                              "\"holder\": \"Vardenis Pavardenis\", " +
-                              "\"exp_year\": 2018," +
-                              "\"exp_month\": 9, " +
-                              "\"cvv\": \"123\" " +
-                              "}";
+                string json = Newtonsoft.Json.JsonConvert.SerializeObject(paymentInfo);
 
                 streamWriter.Write(json);
                 streamWriter.Flush();
@@ -140,18 +101,20 @@ namespace Eshop.Controllers
             {
                 var result = streamReader.ReadToEnd();
                 var paymentResponseJson = JObject.Parse(result);
-                paymentResponseInfo.Amount = (int)paymentResponseJson["amount"];
+                //paymentResponseInfo.Amount = (int)paymentResponseJson["amount"];
                 paymentResponseInfo.Created_At = (DateTime)paymentResponseJson["created_at"];
                 paymentResponseInfo.Id = (string)paymentResponseJson["id"];
-                paymentResponseInfo.Holder = (string)paymentResponseJson["holder"];
-                paymentResponseInfo.Exp_Year = (int)paymentResponseJson["exp_year"];
-                paymentResponseInfo.Exp_Month = (int)paymentResponseJson["exp_month"];
-                paymentResponseInfo.Cvv = (string)paymentResponseJson["cvv"];
+                //paymentResponseInfo.Holder = (string)paymentResponseJson["holder"];
+                //paymentResponseInfo.Exp_Year = (int)paymentResponseJson["exp_year"];
+                //paymentResponseInfo.Exp_Month = (int)paymentResponseJson["exp_month"];
+                //paymentResponseInfo.Cvv = (string)paymentResponseJson["cvv"];
             }
             //*****payment made******
             
-            newOrder.Confirmed = true; //nezinau ar ji tiesiog padaryt conformed ar kokia cia logika turetu buti
+            //confirmed atskirai adminui skirta, o ne pirkimo metu
+            //newOrder.Confirmed = true; //nezinau ar ji tiesiog padaryt conformed ar kokia cia logika turetu buti
             newOrder.PaymentId = paymentResponseInfo.Id;
+            newOrder.PaymentDate = paymentResponseInfo.Created_At;
             newOrder = _ordersRepository.Update(newOrder);
 
             if (newOrder.Confirmed)
@@ -225,6 +188,40 @@ namespace Eshop.Controllers
             }
 
             return NotFound($"Order {confirmedOrder.Id} does not exist in the database");
+        }
+
+        private bool ValidCreditCardNumber(string number)
+        {
+            // accept only digits, dashes or spaces
+            Regex regex = new Regex(@"^[(0-9)-/s]+$");
+            if (!regex.IsMatch(number)) return false;
+
+            number = number.Replace("/", "").Replace(" ", "").Replace("-", "");
+
+            if (number.Length != 16) return false;
+
+            // The Luhn Algorithm
+            int sumOfDigits = number.Where((e) => e >= '0' && e <= '9')
+                .Reverse()
+                .Select((e, i) => ((int)e - 48) * (i % 2 == 0 ? 1 : 2))
+                .Sum((e) => e / 10 + e % 10);
+
+            return (sumOfDigits % 10) == 0;
+        }
+
+        private bool ValidCvv(string cvv)
+        {
+            Regex regex = new Regex(@"^\d{3}$");
+            if (regex.IsMatch(cvv))
+                return true;
+            return false;
+        }
+
+        private bool ValidExpirationDate(int year, int month)
+        {
+            if (year < DateTime.Now.Year) return false;
+            if (month < DateTime.Now.Month) return false;
+            return true;
         }
     }
 
