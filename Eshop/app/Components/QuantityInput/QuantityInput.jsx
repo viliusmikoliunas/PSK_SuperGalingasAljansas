78061271f0540d3306c20ca58308193b0b819a71
Vilius Mikoliūnas <vilius.mikoliunas@gmail.com>
2018-05-29 14:35:27 +0300

    quantity input made smaller

--- Eshop/app/Components/QuantityInput/QuantityInput.jsx
+++ Eshop/app/Components/QuantityInput/QuantityInput.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {Button,InputGroup,InputGroupAddon,Input} from 'reactstrap'
+import './QuantityInputStyles.css'
 
 
 class QuantityInput extends Component{
