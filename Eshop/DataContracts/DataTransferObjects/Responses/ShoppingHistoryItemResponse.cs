1fa89f6a42c11a20b1e143b28031158c08ab8395
Vilius Mikoliūnas <vilius.mikoliunas@gmail.com>
2018-05-29 15:56:08 +0300

    back end sends compressed shopping history

---
+++ Eshop/DataContracts/DataTransferObjects/Responses/ShoppingHistoryItemResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Eshop.Data.Entities;
+
+namespace Eshop.DataContracts.DataTransferObjects.Responses
+{
+    public class ShoppingHistoryItemResponse
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public IEnumerable<OrderedItemDto> Items { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
